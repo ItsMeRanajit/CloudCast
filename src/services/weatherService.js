b66c22d18ed33b11d3b4057d20a80b02c37a094f
ItsMeRanajit <ranajit104das@gmail.com>
2024-12-05 22:20:03 +0530

    ifx2

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.REACT_APP_WEATHER_API; // your api key is here
+const API_KEY = process.env.WEATHER_API; // your api key is here
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 import { DateTime } from "luxon";
 const getWeatherData = async (infotype, searchParams) => {
